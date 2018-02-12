acaa8b48224e51e378f621b937082236237edf0b
vbonvin <vivien.bonvin@epfl.ch>
2018-02-12 09:00:16 +0100

    adding setup and init file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+	name='POUET',
+	version='0.2',
+	description='Programming Observation Usefully at the Euler Telescope ',
+	packages=["pouet", "pouet.obsprogram"]
+)
+
