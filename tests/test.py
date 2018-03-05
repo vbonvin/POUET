c9f0d0a5217e40406a4df9762807d12ae9dc14a1
vbonvin <vivien.bonvin@epfl.ch>
2018-03-05 19:10:00 +0100

    now a working test

--- tests/test.py
+++ tests/test.py
@@ -8,7 +8,13 @@ from astropy.time import Time
 import os, sys
 path = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), '../pouet')
 sys.path.append(path)
+
+# symlink config and obprogram
+os.system("ln -s ../pouet/config .")
+os.system("ln -s ../pouet/obsprogram .")
+
 import clouds, main, meteo, obs, plots, run, util
+
 
 
 logging.basicConfig(format='PID %(process)06d | %(asctime)s | %(levelname)s: %(name)s(%(funcName)s): %(message)s',
@@ -39,3 +45,9 @@ currentmeteo.update(obs_time=Time.now())
 
 # newtime
 # todo create a new time object, play with it
+
+
+
+# remove links
+os.system("unlink config")
+os.system("unlink obsprogram")
